5109892887d409b9d4ffc806dc5a6635084aa7b9
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2022-09-09 09:13:53 -0300

    queue: use RwLock to avoid unecessary write blocks

--- src/server/message_queue.rs
+++ src/server/message_queue.rs
@@ -1,16 +1,18 @@
 use std::{collections::HashMap, sync::Arc};
 
 use bytes::Bytes;
-use tokio::sync::Mutex;
+use tokio::sync::{Mutex, RwLock};
 
 use super::{group::Group, OzResult, OzesConnection};
 
 pub type OzesConnections = Vec<Arc<OzesConnection>>;
 
 #[derive(Default)]
+struct QueueWrapper(RwLock<HashMap<String, Arc<InnerQueue>>>);
 
+#[derive(Default)]
 pub struct MQueue {
-    queues: HashMap<String, Arc<InnerQueue>>,
+    queues: QueueWrapper,
 }
 
 struct InnerQueue {
@@ -26,7 +28,7 @@ impl InnerQueue {
 
 impl MQueue {
     pub async fn add_listener(
-        &mut self,
+        &self,
         connection: Arc<OzesConnection>,
         queue_name: &str,
         group_name: &str,
@@ -38,8 +40,8 @@ impl MQueue {
 
         //let mut queues_write = self.queues.write().await;
         let mut founded = false;
-        if self.queues.contains_key(queue_name) {
-            let inner_queue = Arc::clone(self.queues.get(queue_name).unwrap());
+        if self.queues.contains_key(queue_name).await {
+            let inner_queue = self.queues.get(queue_name).await.unwrap();
             let mut groups_write = inner_queue.groups.lock().await;
             for group in groups_write.iter_mut() {
                 if group.name() == group_name {
@@ -68,13 +70,17 @@ impl MQueue {
             groups: Default::default(),
         };
         log::info!("adding group {group_name} to queue {queue_name}");
-        self.queues
-            .insert(queue_name.to_string(), Arc::new(inner_queue));
+        self.queues.insert(queue_name, Arc::new(inner_queue)).await;
 
         if connection.ok_subscribed().await.is_err() {
             return;
         }
-        self.queues.get(queue_name).unwrap().push_group(group).await;
+        self.queues
+            .get(queue_name)
+            .await
+            .unwrap()
+            .push_group(group)
+            .await;
 
         log::info!("listener add to queue {queue_name} with group {group_name}");
     }
@@ -82,7 +88,7 @@ impl MQueue {
     pub async fn send_message(&mut self, message: Bytes, queue_name: Bytes) -> OzResult<()> {
         let queue_name = String::from_utf8_lossy(&queue_name[..]);
         log::info!("checking if {queue_name} exists",);
-        if let Some(queue) = self.queues.get(&*queue_name) {
+        if let Some(queue) = self.queues.get(&*queue_name).await {
             let mut groups = queue.groups.lock().await;
             log::info!(
                 "queue {} founded, iter over {} groupus",
@@ -98,8 +104,23 @@ impl MQueue {
             };
             log::info!("adding new queue {queue_name}");
             self.queues
-                .insert(queue_name.to_string(), Arc::new(inner_queue));
+                .insert(&*queue_name, Arc::new(inner_queue))
+                .await;
         }
         Ok(())
+    }
+}
+
+impl QueueWrapper {
+    async fn contains_key(&self, key: &str) -> bool {
+        self.0.read().await.contains_key(key)
+    }
+
+    async fn get(&self, key: &str) -> Option<Arc<InnerQueue>> {
+        self.0.read().await.get(key).map(Arc::clone)
+    }
+
+    async fn insert(&self, key: &str, value: Arc<InnerQueue>) {
+        self.0.write().await.insert(key.to_string(), value);
     }
 }
